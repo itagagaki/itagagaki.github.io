46a3c9bbbca82e1cf3eba2088e1316f3b7ff6544
itagagaki <itagagaki@gmail.com>
2023-09-10 08:31:28 +0900

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+window.document.onkeydown = (e) => {
+  	if (e.keyCode === 13) {
+        let element = document.getElementById("answer");
+        if (element.value === "kst"){
+            alert("success");
+        }
+	}  
+}
