4885dbe9b8cb4bcc07dd878c0659391c4277bdfd
itagagaki <itagagaki@gmail.com>
2023-09-10 09:03:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.onkeydown = function (e) {
   console.log('keydown');
   if (e.keyCode === 13) {
     let element = document.getElementById("answer");
+    console.log('element');
+    console.log(element);
     if (element.value === "kst"){
       alert("success");
     }
